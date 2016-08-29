7f7f00bf7eed951fb11269dd07479e6de19ad6ae
Eric Wooley <ericwooley@gmail.com>
2016-08-28 19:09:11 -0700

    updated blueprints for i18n

--- blueprints/component/files/src/app/components/__name__/__name__Component.tsx
+++ blueprints/component/files/src/app/components/__name__/__name__Component.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
-
+import i18nProvider from '../../helpers/I18nProvider'
+import {Ii18nState} from '../../redux/modules/i18n/i18nReducer.generated.ts'
 // This is often useful, delete if you don't need it
 // import { Link } from 'react-router'
 // import { reducers } from '../../redux/reducers.ts'
@@ -13,13 +14,13 @@ export interface I<%= pascalEntityName %>Callbacks {
 }
 
 export type I<%= pascalEntityName %>PropTypes = I<%= pascalEntityName %>Props & I<%= pascalEntityName %>Callbacks
-export default function <%= pascalEntityName %> (props: I<%= pascalEntityName %>Props) {
+function <%= pascalEntityName %> (props: I<%= pascalEntityName %>Props & Ii18nState) {
   const s = require('./<%= camelEntityName %>Component.css')
   return (
       <div className={s.<%= camelEntityName %>}>
-        This sure is a nice new component!
+        {props.strings.components.header.links.About}
       </div>
     )
 }
 
-export {<%= pascalEntityName %> }
+export default i18nProvider({<%= pascalEntityName %> })
