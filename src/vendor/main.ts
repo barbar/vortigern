d85b86e397201836a01d4d4322f4de04d7cdd817
Ben Herila <ben+gh@herila.net>
2017-06-22 08:25:21 +0000

    Update main.ts

--- src/vendor/main.ts
+++ src/vendor/main.ts
@@ -1,6 +1,6 @@
 /**
- * This is an entry point for additional assets,
- * require your assets under this file.
+ * This is an entry point for additional assets, require your assets under this file.
  * Example:
- * require('./bootstrap/css/bootstrap.min.css');
+ *   require('./bootstrap/css/bootstrap.min.css');
+ * If you try using bootstrap 4, see: https://github.com/shakacode/bootstrap-loader/issues/244
  */
